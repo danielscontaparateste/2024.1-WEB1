b99f0520f8ca7fb0c6c6ac31ae7d98fd4a85d802
Daniel Ferreira <danielse@ifce.edu.br>
2024-07-29 17:37:37 -0300

    Aula 29/07

--- projeto9/js/script.js
+++ projeto9/js/script.js
@@ -7,16 +7,30 @@ function execLaco(){
     // cars[4] = 4;
     // cars[5] = 5;
 
-    const qtd = 10;
-
+    const qtd = parseInt(document.querySelector("#iptQtd").value);
+    
+    // Exercício: Substituir o laço for por while e do-while.
     for (let k=0; k < qtd; k++){
-        cars[k] = k / 10;
+        // cars[k] = k / 10;
+        cars[k] = k;
     }
 
     // var cars = ["Ford Ka", "Fiat Palio", "Renault Clio"];
 
+    const spnRes = document.querySelector("#spnRes");
+
+    
+    var str = "";
+    // Exercício: Substituir o laço for por while e do-while.
     for (let k=0; k < cars.length; k++){
-        console.log(cars[k]);
+        str = str + "<br>" + cars[k];
+        // console.log(cars[k]);
     }
 
+    console.log(str);
+    spnRes.innerHTML = str;
+
+
 }
+
+
